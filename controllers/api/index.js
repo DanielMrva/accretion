3bd416460e90c9e063e2244f086cc2dcab891d4b
Alfred Garraffa <agarraffa@gmail.com>
2022-04-02 13:28:23 -0600

    pushing initial routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-
+const pubRoutes = require('./publication-routes');
+const empRoutes = require('./employee-routes');
+const mediaRoutes = require('./media-routes');
+const officeRoutes = require('./office-routes');
 
 router.use('/users', userRoutes);
+router.use('/publications', pubRoutes);
+router.use('/employees', empRoutes);
+router.use('/media', mediaRoutes);
+router.use('/offices', officeRoutes);
 
 
 module.exports = router;
