d16d020332da03c24ddad3760d676387d2c177b3
Amber Zimmerman <ajz1221@gmail.com>
2022-04-05 00:07:13 -0600

    adding in navigation to form page

---
+++ html/script.js
@@ -0,0 +1,12 @@
+function openNav () {
+    document.getElementById('sideNav').style.marginLeft = '0';
+    document.getElementById('rightMain').style.marginLeft = '300px';
+    document.getElementById('expandArrow').style.marginLeft = "300px";
+  };
+
+
+  function closeNav () {
+    document.getElementById('sideNav').style.marginLeft = '-300px';
+    document.getElementById('rightMain').style.marginLeft = '0';
+    document.getElementById('expandArrow').style.marginLeft = "0";
+  }s
