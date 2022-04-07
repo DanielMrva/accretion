d69446485c3015a7e1550d6e4135c48ef7bef4ad
Amber Zimmerman <ajz1221@gmail.com>
2022-04-06 20:40:25 -0600

    finish user dashboard

--- html/script.js
+++ html/script.js
@@ -1,6 +1,6 @@
 function openNav () {
     document.getElementById('sideNav').style.marginLeft = '0';
-    document.getElementById('rightMain').style.marginLeft = '300px';
+    document.getElementById('rightMain').style.marginLeft = '130px';
     document.getElementById('expandArrow').style.marginLeft = "300px";
   };
 
