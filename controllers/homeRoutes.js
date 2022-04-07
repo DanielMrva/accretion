f2f2e51449792b35261fa37f61b79cdab9b5f1f1
Amber Zimmerman <ajz1221@gmail.com>
2022-04-06 23:34:52 -0600

    trying to add function to hide and display login items

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,40 +2,11 @@ const router = require('express').Router();
 const { User } = require ('../models');
 const withAuth = require('../utils/auth');
 
-<<<<<<< HEAD
-// router.get('/', withAuth, async (req, res) => {
-//     try {
-//        const userData = await User.findAll({
-//            attributes: { exclude: ['password'] },
-//            order: [['last_name', 'ASC']],
-//        });
-
-//        const users = userData.map((project) => project.get({ plain: true }));
-
-       
-//        res.render('homepage', {
-//            users,
-//            logged_in: req.session.logged_in,
-//        });
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
-
-
-=======
-<<<<<<< HEAD
-router.get('/login', (req, res) => {
-    if (res.session.logged_in) {
-        res.redirect('/');
-        return;
-=======
 router.get('/', async (req, res) => {
     try {
       res.render('homepage')
     } catch (err) {
       res.status(500).json(err);
->>>>>>> 6c43b80f1dd8bbf2a0ea9659d588989ffbe1fe7d
     }
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
+// const loginContainer = document.getElementById("login");
+// const loginBtn = document.getElementById("loginBtn");
+
+// loginBtn.onclick = function () {
+//     if (loginContainer.style.display !== "none") {
+//         loginContainer.style.display = "none";
+//     } else {
+//         loginContainer.style.display = "block";
+//     }
+// };
+
 function openNav () {
     document.getElementById('sideNav').style.marginLeft = '0';
     document.getElementById('rightMain').style.marginLeft = '130px';
