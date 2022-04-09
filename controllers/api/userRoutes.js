682fc9a3a5274b0db4ace246ae1d3f200d52e25f
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-04-09 10:39:45 -0600

    commented line 17 and line 48 in userRoutes back into the code so that the login and sign up will work. they are connected in the homeroutes and must be utilized

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
     });
 
     req.session.save(() => {
-      // req.session.user_id = userData.id;
+      req.session.user_id = userData.id;
       req.session.logged_in = true;
       res.status(200).json(userData);
     });
@@ -45,7 +45,7 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
-      // req.session.user_id = userData.id;
+      req.session.user_id = userData.id;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
