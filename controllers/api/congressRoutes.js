ce61626e152cb9eea011b5d186a8045438c6dc83
Alfred Garraffa <agarraffa@gmail.com>
2022-04-09 20:42:07 -0600

    updated routes and continued FE logic work

--- controllers/api/congressRoutes.js
+++ controllers/api/congressRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Congress, User } = require('../../models');
+const { Congress, Office } = require('../../models');
 
 //endpoint: /api/congress
 //gets a list of all congressional hearings in database
 router.get('/', async (req, res) => {
     try {
-      const congressData = await Congress.findAll({include: [{model: User}]
+      const congressData = await Congress.findAll({
+        include: [{model: Office}]
       });
       res.status(200).json(congressData);
     } catch (err) {
@@ -17,7 +18,8 @@ router.get('/', async (req, res) => {
 //gets one congressional hearing, needs id 
 router.get('/:id', async (req, res) => {
 try {
-    const congressData = await Congress.findByPk(req.params.id, {include: [{model: User}]
+    const congressData = await Congress.findByPk(req.params.id, {
+      include: [{model: Office}]
     });
     if (!congressData) {
     res.status(404).json({ message: 'No congressional hearings found with this id!' });
@@ -36,21 +38,52 @@ router.post('/', async (req, res) => {
       const congressData = await Congress.create(req.body);
       res.status(200).json(congressData);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
     }
   });
 
 //endpoint: /api/congress/:id
 //updates one congressional hearing, needs id
 router.put('/:id', async (req, res) => {
+
+  let data = {};
+
     try {
+
+        if (req.body.rep_committee) {
+          data.rep_committee = req.body.rep_committee;
+        }
+
+        if (req.body.desc) {
+          data.desc = req.body.desc;
+        }
+        
+        if (req.body.employee_name) {
+          data.employee_name;
+        }
+
+        if (req.body.employee_email) {
+          data.employee_email = req.body.employee_email
+        }
+
+        if (req.body.office_id) {
+          data.office_id = req.body.office_id;
+        }
+
         const congressData = await Congress.update(
-        {reps_comm: req.body.reps_comm},
+        data,
         {where: {id: req.params.id}}
         );
+
+        if (!congressData) {
+          res.status(404).json({message: 'Could not find that record'})
+          return;
+        }
+
         res.status(200).json(congressData);
+
     } catch (err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
 });
 
--- public/js/form.js
+++ public/js/form.js
@@ -1,52 +1,9 @@
-// const { goGetter, posterChild, updater, terminator } = require('./fetch')
 
 const pubPost = document.getElementById('submitPublications');
 
-// let url = 'http://localhost:3001/api/'
-
-
-// work with optional query info in the url to accomplish. create those in the routes
-
-
-async function fetcher(method, category, id=undefined, body=undefined) {
-
-    let data;
-
-    switch (method) {
-
-        case 'GET':
-            
-            if (!id) {
-
-                data = await goGetter(category);
-                return data;
-
-            } else {
-
-                data = await goGetter(category, id);
-                return data;
-            }
-        
-        case 'POST':
-
-            data = await posterChild(category, body);
-            return;
-
-        case 'PUT': 
-
-            data = await updater(category, body, id);
-            return;
-
-        case 'DELETE':
-            
-            data = await terminator(category, id)
-            return;
-    }
-
-}
-
 
 // currently this is for the publication table
+// replicate this for each entry type
 async function pubSubmit(event) {
 
     event.preventDefault();
@@ -54,13 +11,22 @@ async function pubSubmit(event) {
     // defines the data
     let pubData = {
         pub_name: document.getElementById("pubName").value,
-        pub_date: document.getElementById("pubDate").value,
-        article_title: document.getElementById("title").value,
-        name: document.getElementById("authName").value,
-        other_contrib: document.getElementById("contributor").value,
-        user_id: 1,
-        office_id: 1
+        title: document.getElementById("title").value,
+        desc: document.getElementById("desc").value,
+        employee_name: document.getElementById("name").value,
+        employee_email: document.getElementById("email").value,
+        authors: document.getElementById("authors").value,
+        // office needs to be a dropdown of all the offices, which in turn queries for the id of that office abbrev/name
     }
+
+
+    // this converts the office selected to an id for use in the post. Have the default office be the office of the user.
+    // let office = document.getElementById("office");
+
+    // let offID = await fetch(`/api/offices/abbrev/${office}`);
+
+    // pubData.office_id = offID.id;
+
 
     // assigns a null value to any empty fields
     for (const property in pubData) {
