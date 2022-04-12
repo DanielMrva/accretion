b381bfb7956d0ca63685422a88cf59c7fa82440c
Amber Zimmerman <ajz1221@gmail.com>
2022-04-11 19:12:42 -0600

    commit before push

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,8 @@ const signUpBtn = document.getElementById("signUpBtn");
 const square = document.querySelector(".square");
 const squareOne = document.querySelector(".square-1");
 const squareTwo = document.querySelector(".square-2");
-
+const expandArrow = document.getElementById("expandArrow");
+const sideNav = document.getElementById("sideNav");
 
 
 // loginBtn.onclick = function () {
@@ -32,13 +33,21 @@ function displayLogin () {
 function openNav () {
     document.getElementById('sideNav').style.marginLeft = '0';
     document.getElementById('rightMain').style.marginLeft = '130px';
-    document.getElementById('expandArrow').style.marginLeft = "300px";
+    document.getElementById('expandArrow').style.marginLeft = "320px";
 };
 
 
 function closeNav () {
-  document.getElementById('sideNav').style.marginLeft = '-300px';
+  document.getElementById('sideNav').style.marginLeft = '-320px';
   document.getElementById('rightMain').style.marginLeft = '0';
   document.getElementById('expandArrow').style.marginLeft = '0';
 };
+
+function navMove () {
+    if (sideNav.style.marginLeft === "-320px") {
+        openNav();
+    } else {
+        closeNav();
+    }
+}
 
